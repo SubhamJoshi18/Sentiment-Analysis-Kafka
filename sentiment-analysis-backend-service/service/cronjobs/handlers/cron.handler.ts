224387b45eb7019404363f944c45ef79e65f0850
SubhamJoshi18 <shubhamrajjoshi69@gmail.com>
2025-07-14 22:19:45 +0545

    integarte the hash map for the api fetching

--- sentiment-analysis-backend-service/service/cronjobs/handlers/cron.handler.ts
+++ sentiment-analysis-backend-service/service/cronjobs/handlers/cron.handler.ts
@@ -4,15 +4,31 @@ import { apiUrl } from "../../constant/api.constant";
 
 class CronHandler {
   public requestHelper: RequestHelper;
+  public apiHashMap: Record<string, Record<string, any>> = {};
 
   constructor() {
     this.requestHelper = new RequestHelper(apiUrl);
+    this.apiHashMap = {
+      [`${apiUrl}`]: {},
+    };
   }
 
   public async evaluateTheProcess() {
     try {
       const response = await this.requestHelper.getRequest();
-      console.log(response);
+
+      const apiData = response?.data;
+
+      if (Object.prototype.hasOwnProperty.call(apiData, "articles")) {
+        if (Object.keys(this.apiHashMap).includes(apiUrl)) {
+          this.apiHashMap[apiUrl] = {
+            data: apiData.articles,
+            totalData: apiData.totalResults,
+          };
+        }
+      }
+      const payload = JSON.parse(JSON.stringify(this.apiHashMap));
+      console.log(payload);
     } catch (err: UnknownAny) {
       throw new Error(err);
     }
